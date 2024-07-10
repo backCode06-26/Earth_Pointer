62866d31cba90f24f17ee6ed9ce425603bab1e55
Yoohojin <118101197+backCode06-26@users.noreply.github.com>
2024-07-10 14:32:47 +0900

    Add files via upload

---
+++ Earth_Pointer/src/main/java/com/earth_pointer/EarthPointerApplication.java
@@ -0,0 +1,13 @@
+package com.earth_pointer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthPointerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthPointerApplication.class, args);
+    }
+
+}
---
+++ Earth_Pointer/src/test/java/com/cos/earth_pointer/EarthPointerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.earth_pointer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthPointerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
