9641c0cc1dd85642ba3e1d8d03d07d61bd7abdb6
Yoohojin <118101197+backCode06-26@users.noreply.github.com>
2024-07-15 08:56:47 +0900

    Add files via upload

--- Earth_Pointer/src/main/java/com/earth_pointer/repository/PostRepository.java
+++ Earth_Pointer/src/main/java/com/earth_pointer/repository/PostRepository.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 @Repository
 public class PostRepository {
@@ -23,6 +23,7 @@ public class PostRepository {
         this.databaseUtils = databaseUtils;
     }
 
+    // 게시글 작성하기
     public void addPost(String email, Post post) {
         Connection conn = null;
         PreparedStatement ps = null;
@@ -48,6 +49,7 @@ public class PostRepository {
         }
     }
 
+    // 게시글 수정하기
     public void updatePost(int postId, String email, Post post) {
         Connection conn = null;
         PreparedStatement ps = null;
@@ -71,6 +73,7 @@ public class PostRepository {
         }
     }
 
+    // 게시글 삭제하기
     public void deletePost(int postId) {
         Connection conn = null;
         PreparedStatement ps = null;
@@ -90,5 +93,82 @@ public class PostRepository {
         } finally {
             DatabaseUtils.close(conn, ps, rs);
         }
+    }
+
+    // 사용자 게시글 가져오기
+    public ArrayList<Post> getAllPosts(String email) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<Post> posts = new ArrayList<>();
+
+        try {
+            conn = dataSource.getConnection();
+
+            String sql = "select * from posts where user_id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, databaseUtils.getUserIdByEmail(email));
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Post post = new Post(
+                        rs.getInt("post_id"),
+                        rs.getInt("user_id"),
+                        rs.getString("title"),
+                        rs.getString("content"),
+                        rs.getTimestamp("created_at")
+                );
+                posts.add(post);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            DatabaseUtils.close(conn, ps, rs);
+        }
+        return posts;
+    }
+
+    // 게시글 10개씩 가져오기
+    public ArrayList<Post> getAllPostsWithPagination(int page) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<Post> posts = new ArrayList<>();
+
+        try {
+            conn = dataSource.getConnection();
+
+            int startRow = page*10-9;
+            int endRow = 10*page;
+
+            String sql = "select * from ( " +
+                    "    select ROWNUM NUM, P.* from (" +
+                    "       select * from posts order by post_id desc" +
+                    "   ) P" +
+                    ") where NUM between ? and ?";
+
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, startRow);
+            ps.setInt(2, endRow);
+
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+                Post post = new Post(
+                        rs.getInt("post_id"),
+                        rs.getInt("user_id"),
+                        rs.getString("title"),
+                        rs.getString("content"),
+                        rs.getTimestamp("created_at")
+                );
+                posts.add(post);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            DatabaseUtils.close(conn, ps, rs);
+        }
+        return posts;
     }
 }
