45457e7ea972183b758454c357beec362660f328
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-07-23 11:49:31 +0900

    Update RecyclingInfoRepository.java

--- Earth_Pointer/src/main/java/com/earth_pointer/repository/RecyclingInfoRepository.java
+++ Earth_Pointer/src/main/java/com/earth_pointer/repository/RecyclingInfoRepository.java
@@ -1,6 +1,6 @@
 package com.earth_pointer.repository;
 
-import com.earth_pointer.domain.recyclingInfos.RecyclingInfo;
+import com.earth_pointer.domain.RecyclingInfo;
 import com.earth_pointer.utils.DatabaseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
